65b1e46a35c06b99e50e59a2016fa021c40ae190
Conscientious Technologies <167194750+CT-Dev-User@users.noreply.github.com>
2025-04-25 23:53:16 +0530

    changed backend

--- models/instructor.js
+++ models/instructor.js
@@ -1,32 +1,4 @@
 import mongoose from 'mongoose';
-
-// const payoutSchema = new mongoose.Schema({
-//   instructorId: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: 'User',
-//     required: true
-//   },
-//   amount: {
-//     type: Number,
-//     required: true
-//   },
-//   status: {
-//     type: String,
-//     enum: ['pending', 'processed', 'rejected'],
-//     default: 'pending'
-//   },
-//   paymentType: {
-//     type: String,
-//     default: 'UPI'
-//   },
-//   dateRequested: {
-//     type: Date,
-//     default: Date.now
-//   },
-//   dateProcessed: Date
-// });
-
-
 // Update the status enum to include 'approved'
 const payoutSchema = new mongoose.Schema({
   instructorId: {
@@ -40,7 +12,7 @@ const payoutSchema = new mongoose.Schema({
   },
   status: {
     type: String,
-    enum: ['pending', 'processed', 'approved', 'rejected'],
+    enum: ['pending', 'processed', 'approved', 'rejected','deleted'],
     default: 'pending'
   },
   paymentType: {
@@ -51,7 +23,8 @@ const payoutSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   },
-  dateProcessed: Date
+  dateProcessed: Date,
+  dateDeleted: Date
 });
 
 export default mongoose.model('Payout', payoutSchema);
